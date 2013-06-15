007782dcf8ec8e5514fd0f3279415474ff84faf2
wes <wes@ubuntu.(none)>
2013-06-15 15:01:44 -0500

    wall_crawl added containing algorithms to crawl users walls and store data in an object

--- mayors_tools/run_queries.py
+++ mayors_tools/run_queries.py
@@ -57,6 +57,7 @@ def run_queries(campaign_id):
 		set_of_primary_event_types = set(cur_primary_event_types)
 		count_of_event_types = {}
 		# count the actions and update our data object accordingly
+		count_of_event_types = {action: cur_primary_event_types.count(action) for action in set_of_primary_event_types}
 		for action in set_of_primary_event_types:
 			count_of_event_types[action] = cur_primary_event_types.count(action)
 		data_object["data"][i][primaries[i]]["actions"].update(count_of_event_types)
@@ -70,4 +71,72 @@ def run_queries(campaign_id):
 		string_timestamp = datetime.fromtimestamp(int(timestamp)).strftime('%Y-%m-%d %H:%M:%S')
 		print "No new data since %s has been found" % string_timestamp
 	return data_object
+
+
+
+
+def run_queries2(campaign_id):
+	data_object = {"data":[]}
+	try:
+		time_file = open('timestamp.txt','r')
+		timestamp = time_file.read()
+		time_file.close()
+		os.remove('timestamp.txt')
+		query = "SELECT fbid,friend_fbid,type,udpated FROM events WHERE updated > FROM_UNIXTIME({0}) AND campaign_id={1}".format(timestamp,str(campaign_id))
+
+	except IOError:
+		query = "SELECT fbid,friend_fbid,type,updated FROM events WHERE campaign_id={0}".format(str(campaign_id))
+
+	con = mysql.connect('edgeflip-db.efstaging.com','root','9uDTlOqFmTURJcb','edgeflip')
+	cur = con.cursor()
+	our_orm = PySql(cur)
+	# use the query defined above to get the events
+	all_pertinent_events = our_orm.query(query)
+	if all_pertinent_events != None:
+		p_events = [event for event in all_pertinent_events if event[0] != None]
+	
+		for i in range(len(p_events)):
+			data_object["data"].append({})
+			data_object["data"][i][p_events[i][0]] = {}
+			primary_query = "SELECT fname,lname,email,gender,birthday,city,state FROM users WHERE fbid={0}".format(p_events[i][0])
+			primary_info = our_orm.query(primary_query)
+			# add key value pairs of all our primary's personal data
+			data_object["data"][i][p_events[i][0]]["fname"] = primary_info[0][0]
+			data_object["data"][i][p_events[i][0]]["lname"] = primary_info[0][1]
+			data_object["data"][i][p_events[i][0]]["email"] = primary_info[0][2]
+			data_object["data"][i][p_events[i][0]]["gender"] = primary_info[0][3]
+			data_object["data"][i][p_events[i][0]]["birthday"] = primary_info[0][4]
+			data_object["data"][i][p_events[i][0]]["city"] = primary_info[0][5]
+			data_object["data"][i][p_events[i][0]]["state"] = primary_info[0][6]
+			# action data
+			data_object["data"][i][p_events[i][0]]["action"] = {}
+#			data_object["data"][i][p_events[i]]["action"][p_events[i][2]] = {}
+			#data_object["data"][event[0]]["action"][event[2]][
 			
+			# secondary handling
+			if p_events[i][1] != None:
+				data_object["data"][i][p_events[i][0]]["action"][p_events[i][2]] = {}
+				s_query = "SELECT fname,lname,email,gender,birthday,city,state FROM users WHERE fbid={0}".format(p_events[i][1])
+				s_info = our_orm.query(s_query)
+
+				data = {p_events[i][1]: {"fname": s_info[0][0], "lname": s_info[0][1], "email": s_info[0][2], "gender": s_info[0][3], "birthday": s_info[0][4], "city": s_info[0][5], "state": s_info[0][6]}}
+				data_object["data"][i][p_events[i][0]]["action"][p_events[i][2]].update(data)
+			
+			else:
+				data_object["data"][i][p_events[i][0]]["action"][p_events[i][2]] = None
+	else:
+		pass
+	f = open('timestamp.txt','w')
+        current_time = int(round(time.time()))
+        f.write(str(current_time))
+        f.close()
+        # in case we've run our queries and nothing new has been found
+        if len(data_object["data"]) == 0:
+                # format the timestamp
+                string_timestamp = datetime.fromtimestamp(int(timestamp)).strftime('%Y-%m-%d %H:%M:%S')
+                print "No new data since %s has been found" % string_timestamp
+	return data_object
+
+
+if __name__ == '__main__':
+	run_queries(3)			
---
+++ wall_crawl/crawl_tools.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+# this class relies on the mechanize and cookielib modules
+import mechanize
+import cookielib
+import json
+import MySQLdb as mysql
+from navigate_db import PySql
+import urllib2
+
+# not necessarily needed, but good for testing stuff with my own account and credentials
+
+class GoodBrowser(object):
+	def __init__(self):
+		self.br = mechanize.Browser()
+		self.cj = cookielib.LWPCookieJar()
+	# this method allows us to look like a web browser
+	def add_credentials(self):
+		self.br.set_debug_http(True)
+		self.br.set_debug_redirects(True)
+		self.br.set_handle_robots(False)
+		self.br.set_handle_equiv(True)
+		self.br.set_handle_gzip(True)
+		self.br.set_cookiejar(self.cj)
+		self.br.addheaders = [('User-Agent', 'Mozilla/5.0(X11;U;Linux i686;en-US;rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
+
+	def go(self,url):
+		self.br.open(url)
+
+	def facebook_login(self):
+		self.go('http://www.facebook.com')
+		self.br.select_form(nr=0)
+		email = raw_input('facebook email: ')
+		password = raw_input('facebook password: ')		
+		self.br.form['email'] = email
+		self.br.form['pass'] = password
+		self.br.submit()
+		print "\n+++++ Logged into facebook as %s +++++\n" % email
+
+	# need to have visited the graph api explorer and generated a token
+	def graph_query(self, fbid, fields, access_token):
+		graph_api = 'https://graph.facebook.com/{0}?fields={1}&access_token={2}'
+
+		formatted = graph_api.format(fbid,fields,access_token)
+		# make query
+		self.go(formatted)
+		json_response = json.loads(self.br.response().read())
+		return json_response
+
+
+# algorithm to crawl everyones feeds and compile some data objects relating to
+# those feeds
+# an object with the most recent post's "updated_time" is stored and written
+# to a file so that we can pull those user-specific times out when we call
+# crawl_all_feeds the next time
+
+
+def crawl_all_feeds():
+	data_object = {"data": []}
+	time_object = {}
+	api = 'https://graph.facebook.com/{0}?fields={1}&access_token={2}'
+	feed = 'feed'
+	con = mysql.connect('edgeflip-db.efstaging.com', 'root', '9uDTlOqFmTURJcb', 'edgeflip')
+	cur = con.cursor()
+	orm = PySql(cur)
+	pertinent_info = orm.query('SELECT fbid,ownerid,token FROM tokens')
+	for i in range(len(pertinent_info)):
+		key = str(pertinent_info[i][0])
+		primary = str(pertinent_info[i][1])
+		access_token = pertinent_info[i][2]
+		cur_data = {key: {}}
+		cur_data[key]["primary_fbid"] = primary
+		formatted_query = api.format(key,feed,access_token)
+		data = json.loads(urllib2.urlopen(formatted_query).read())
+		most_recent_post = data["feed"]["data"][0]["updated_time"]
+		to_update = {key: {primary: most_recent_post}}
+		time_object.update(to_update)
+		data_object["data"].append(data)
+	times_str = json.dumps(time_object)
+	time_file = open('times.py','w')
+	time_file.write('times = %s' % times_str)
+	time_file.close()	
+	return data_object
