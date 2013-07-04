52d786fbeef6813920aa1e662c9c7a178c87e244
wes <wes@wes.(none)>
2013-07-03 19:09:52 -0500

    virginia_send_to_s3 devised and added to our virginia_tools repo as well as incorporated on our ec2 server and run in the cront job.  this method creates an s3 key with the events and reference files as keys and their contents as their values.  an API call to s3.amazonaws.com/virginia_bucket/events_file_month_day_year and s3.amazonaws.com/virginia_bucket/ref_table_month_day_year will yield the desired after files

--- virginia_tools/run_daily3.py
+++ virginia_tools/run_daily3.py
@@ -1,5 +1,15 @@
-#/usr/bin/env python
+#/usr/bin/python
 from generate_data_for_export3 import create_events_file
+from generate_report import generate_daily_report
+import time
+from virginia_send_to_s3 import send_to_s3
+from time import strftime
 
 if __name__ == '__main__':
+	try:
+		timestamp = open('timestamp.txt','r').read()
+	except IOError:
+		timestamp = str(int(time.time()))
+	generate_daily_report(3,timestamp)
 	create_events_file(3)
+	send_to_s3()
