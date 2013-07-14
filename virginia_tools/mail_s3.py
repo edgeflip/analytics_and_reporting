93f911a336110e78867851c2ef22c4b08e7caa5e
wes <wes@wes.(none)>
2013-07-14 12:54:17 -0500

    changes to s3 emails to not attach files and JUST send link made; links wont send with attachment

--- virginia_tools/mail_s3.py
+++ virginia_tools/mail_s3.py
@@ -5,30 +5,36 @@ from email.MIMEText import MIMEText
 from time import strftime
 
 def mail_to_s3():
-	to_send = ['rayid@edgeflip.com','wes@edgeflip.com','wesley7879@gmail.com']
+	to_send = ['wes@edgeflip.com','wesley7879@gmail.com']
 	m = strftime('%m')
 	d = str(int(strftime('%d'))-1)
 	y = strftime('%Y')
 
 	files = ['events_file_{0}_{1}_{2}.csv'.format(m,d,y),'ref_table_{0}_{1}_{2}.csv'.format(m,d,y)]
 
+	msg = MIMEMultipart()
+	msg['From'] = 'wesleymadrigal_99@hotmail.com'
+	mailserver = smtplib.SMTP('smtp.live.com',587)
+	mailserver.ehlo()
+	mailserver.starttls()
+	mailserver.ehlo()
+	mailserver.login('wesleymadrigal_99@hotmail.com','madman2890')
+	
 	for person in to_send:
-		for _file in files:
-			msg = MIMEMultipart()
-			msg['From'] = 'wesleymadrigal_99@hotmail.com'
-			msg['To'] = person
-			msg['Subject'] = 'S3 file %s uploaded' % _file
-			filename = _file
-			f = open(_file,'r').read()
-			attachment = MIMEText(f)
-			attachment.add_header('Content-Disposition','attachment',filename=filename)
-			msg.attach(attachment)
-			mailserver = smtplib.SMTP('smtp.live.com',587)
-			mailserver.ehlo()
-			mailserver.starttls()
-			mailserver.ehlo()
-			mailserver.login('wesleymadrigal_99@hotmail.com','madman2890')
-			mailserver.sendmail('wesleymadrigal_99@hotmail.com',person,msg.as_string())
-			print "%s sent to %s" % (_file, person)
+		msg['To'] = person
+		for i in range(len(files)):
+			msg['Subject'] = 'S3 file %s uploaded' % files[i]
+			filename = files[i]
+			try:
+				f = open(files[i],'r').read()
+				if i == 0:
+					message = 'https://s3.amazonaws.com/virginia_bucket/events_file_{0}_{1}_{2}'.format(m,d,y)
+				else:
+					message = 'https://s3.amazonaws.com/virginia_bucket/ref_table_{0}_{1}_{2}'.format(m,d,y)
+				msg.attach(MIMEText(message))
+				mailserver.sendmail('wesleymadrigal_99@hotmail.com',person,msg.as_string())
+				print "%s sent to %s" % (files[i], person)
+			except IOError:
+				pass
 
 
