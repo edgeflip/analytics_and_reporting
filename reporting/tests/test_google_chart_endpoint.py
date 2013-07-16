6a6b70efc5a1d212ed03e00aabb674db5220e1c9
wes <wes@wes.(none)>
2013-07-16 15:54:15 -0500

    Added a 'reporting' directory to encapsulate all of our reporting stuff and also added a 'tests' directory with a to take as line arguments an integer and a url to slam the url iteratively with requests until we get to the iteration of the passed parameter'

---
+++ reporting/tests/test_google_chart_endpoint.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+import sys
+import requests
+import time
+
+"""
+    Using the requests module we will run a series of 
+    user-specified tests against our newly established
+    endpoint and check it's capacity.  For this test
+    we are using virginia's credentials.
+"""
+
+if __name__ == '__main__':
+    amount = sys.argv[1]
+    if amount.isdigit():
+        start_time = time.time()
+        n = 0
+        for i in range(int(amount)):
+            r = requests.get('http://50.16.226.172?client_id=DXzVnCY4EZ0%3D')
+            if r.status_code == 200:
+                n += 1
+            elif r.status_code != 200:
+                end_time = time.time()
+                total_time = end_time-start_time
+                print "Too much load\n{0} requests in {1} seconds\n".format(str(n),str(int(total_time)))
+                break
+            else:
+                print "%s,%s" % (r.status_code,r.reason)
+                break
+        end_time = time.time()
+        total_time = int(end_time-start_time)
+        print "\n{0} GET requests ran successfully in {1} seconds\n".format(str(n), str(total_time))
+    else:
+        print "We need an integer"
+
+
