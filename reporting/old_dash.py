246a6a27dca47bdf5c2d728cd35d489232355e77
wes <wes@wes.(none)>
2013-08-14 14:15:36 -0500

    fixed a bug in old_dash.py with the template rendering

--- reporting/old_dash.py
+++ reporting/old_dash.py
@@ -45,7 +45,7 @@ def main_handler():
                 second_row = reader.next()
                  
                 today = strftime('%m') + '/' + strftime('%d') + '/' + strftime('%Y')
-                return render_template("old_dash.html", hourly_data=hourly_data, daily_data=daily_data, today=today, visits=str(int(first_row[0])), clicks=str(int(first_row[1])), auths=str(int(first_row[2])), distinct_auths=str(int(first_row[3])), users_shown=str(int(first_row[4])), users_shared=str(int(first_row[5])), friends_shared=str(int(first_row[6])), distinct_friends_shared=str(int(first_row[7])), clickbacks=str(int(first_row[8])), visits2=str(int(second_row[0])), clicks2=str(int(second_row[1])), auths2=str(int(second_row[2])), distinct_auths2=str(int(second_row[3])), users_shown2=str(int(second_row[4])), users_shared2=str(int(second_row[5])), friends_shared2=str(int(second_row[6])), distinct_friends_shared2=str(int(second_row[7])), clickbacks2=str(int(second_row[8])))
+                return render_template("old_temp.html", hourly_data=hourly_data, daily_data=daily_data, today=today, visits=str(int(first_row[0])), clicks=str(int(first_row[1])), auths=str(int(first_row[2])), distinct_auths=str(int(first_row[3])), users_shown=str(int(first_row[4])), users_shared=str(int(first_row[5])), friends_shared=str(int(first_row[6])), distinct_friends_shared=str(int(first_row[7])), clickbacks=str(int(first_row[8])), visits2=str(int(second_row[0])), clicks2=str(int(second_row[1])), auths2=str(int(second_row[2])), distinct_auths2=str(int(second_row[3])), users_shown2=str(int(second_row[4])), users_shared2=str(int(second_row[5])), friends_shared2=str(int(second_row[6])), distinct_friends_shared2=str(int(second_row[7])), clickbacks2=str(int(second_row[8])))
             else:
                 return "We don't have any data for that client_id"
 
@@ -62,8 +62,5 @@ def second_app():
 	return handle_request()
 
 if __name__ == '__main__':
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.bind(('0.0.0.0',80))
-    port = sock.getsockname()[1]
-    sock.close()
-    application.run(host='0.0.0.0',port=port)
+    application.run('0.0.0.0',port=80)
+
