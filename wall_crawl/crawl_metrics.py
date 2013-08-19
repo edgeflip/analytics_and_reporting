cbce881035d8fc81afb8b2390743c2b9feba106e
Wes <wes@wes.(none)>
2013-08-18 22:54:08 -0500

    build_weighted.py and build_weighted_test.py added to crawl_metrics as advancements to edge metrics as well as some small changes in always_crawl_from_database.py and crawl_realtime_updates.py in crawl_tools.py

--- wall_crawl/crawl_metrics.py
+++ wall_crawl/crawl_metrics.py
@@ -1,4 +1,79 @@
 #!/usr/bin/env python
+from con_s3 import connect_s3
+import json
+
+"""
+    build_weighted connects to our metric_bucket and builds metrics around what we have already
+    created during the main crawler systemized data collection process.  this algorithm will
+    leverage previously stored data and advance our understanding of the edges contained
+    therein
+"""
+
+def build_weighted():
+    conn = connect_s3()
+    mets = conn.get_bucket('metric_bucket')
+    for key in mets.list():
+        cur_mets = json.loads(key.get_contents_as_string())
+        ownerid = key.key
+        # get all the posts_ids 
+        post_ids = []
+        for fbid in cur_mets.keys():
+            for each in cur_mets[fbid].keys():
+                for post in cur_mets[fbid][each]:
+                    if post['id'] not in post_ids:
+                        post_ids.append(post['id'])
+                    else:
+                        pass 
+        divisor = float(len(post_ids))
+        if "weights" not in cur_mets.keys():
+            cur_weights = { str(float(sum([len(cur_mets[fbid][k]) for k in cur_mets[fbid].keys()]))/divisor) : fbid for fbid in cur_mets.keys() if fbid != ownerid}
+            cur_mets["weights"] = {}
+            cur_mets["weights"].update(cur_weights)
+        else:
+            cur_weights = { str(float(sum([len(cur_mets[fbid][k]) for k in cur_mets[fbid].keys()]))/divisor) : fbid for fbid in cur_mets.keys() if fbid != ownerid}
+            cur_mets["weights"].updated(cur_weights)
+        key.set_contents_from_string(json.dumps(cur_mets))
+    	print "Connections of %s weighed" % str(ownerid)
+
+
+"""
+   build_weighted_test runs a single requested metric_bucket key and creates the weighted
+   graph of that key within our metric_bucket (in more abstract words the build_weighted_test
+   algorithm advances our known edge relations between a primary and his/her secondaries
+   based on the posts, likes, comments, stories tagged they have in common
+"""
+
+def build_weighted_test(ownerid):
+    conn = connect_s3()
+    metrics = conn.get_bucket('metric_bucket')
+    key = metrics.get_key(ownerid)
+    if key != None:
+        cur_mets = json.loads(key.get_contents_as_string())
+        # get all the posts_ids 
+        post_ids = []
+        for fbid in cur_mets.keys():
+            for each in cur_mets[fbid].keys():
+                for post in cur_mets[fbid][each]:
+                    if post['id'] not in post_ids:
+                        post_ids.append(post['id'])
+                    else:
+                        pass
+        divisor = float(len(post_ids))
+        if "weights" not in cur_mets.keys():
+            cur_weights = { str(float(sum([len(cur_mets[fbid][k]) for k in cur_mets[fbid].keys()]))/divisor) : fbid for fbid in cur_mets.keys() if fbid != ownerid }
+            cur_mets["weights"] = {}
+            cur_mets["weights"].update(cur_weights)
+        else:
+            cur_weights = { str(float(sum([len(cur_mets[fbid][k]) for k in cur_mets[fbid].keys()]))/divisor) : fbid for fbid in cur_mets.keys() if fbid != ownerid }
+            cur_mets["weights"].updated(cur_weights)
+        key.set_contents_from_string(json.dumps(cur_mets))
+        print "Connections of %s weighed" % str(ownerid)
+    else:
+        print "Connections of %s not weight" % str(ownerid)
+
+            
+
+
 
 """
     imetrics takes as parameter a json blob and a ownerid and returns an object with metrics around how connected
@@ -8,7 +83,7 @@
 """
 
 
-def imetrics(resp, ownerid):
+def imetrics(resp, ownerid, fbid):
     try:
         # posts from the cared about user period
         posts_from = []
