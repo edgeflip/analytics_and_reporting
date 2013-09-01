e73fd0630d04d6bb2c905b4652e1b9826bd1f30d
Japhy Bartlett <japhy@edgeflip.com>
2013-08-31 20:54:29 -0500

    pull this stuff in as needed

--- wall_crawl/con_s3.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-
-def connect_s3():
-    from boto.s3.connection import S3Connection
-    conn = S3Connection('AKIAJDIWDVVGWXFOSPEQ', 'RpcwFl6tw2XtOqnwbhXK9PemhUQ8kK6UdCMJ5GaI')
-    return conn
--- wall_crawl/test_environ.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-from navigate_db import PySql as pysql
-
-
-orm = pysql('edgeflip-production-a-read1.cwvoczji8mgi.us-east-1.rds.amazonaws.com', 'root', 'YUUB2ctgkn8zfe', 'edgeflip')
-orm.connect()
