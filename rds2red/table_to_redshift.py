97181e682fef54e14981d338102e0a7d957dc6e0
Japhy Bartlett <japhy@edgeflip.com>
2013-09-18 11:55:08 -0500

    set up a main() function

--- rds2red/table_to_redshift.py
+++ rds2red/table_to_redshift.py
@@ -58,82 +58,83 @@ def up2s3(table):
     print "Uploaded %s to s3" % table        
 
 
-if __name__ == '__main__':
-    if len(sys.argv) == 2:
-        start = time.time()
-        table = sys.argv[1]
-
-        dbcreds = open('creds/dbcreds.txt', 'r').read().split('\n')
-        dbconn = mysql.connect(dbcreds[0], dbcreds[1], dbcreds[2], dbcreds[3])
-        cur = dbconn.cursor()  
-        # get the description
-        cur.execute("describe %s" % table)
-        description = cur.fetchall()
-        # connect to redshift and copy the file that we just uploaded to s3 to redshift
-        engine = create_engine('postgresql+psycopg2://', creator=create_conn)
-        redconn = engine.connect()
-        columns = create_query(description)
-     
-        try:
-            redconn.execute("create table {0}({1})".format(table, columns))
-        except:
-            delete_current = raw_input("%s already exists\nDo you want to delete rewrite?: " % table)
-            if delete_current.lower() == 'y' or delete_current.lower() == 'yes':
-                redconn.execute("drop table %s" % table)
-                time.sleep(1)
-                redconn.execute("create table {0}({1})".format(table, columns))
-            elif delete_current.lower() == 'n' or delete_current.lower() == 'no':
-                print "Process aborted"
-                exit()
-        
-        write2csv(table, cur)
-         
-        csvtime = int( time.time() )
-        runcsv = csvtime - start
-
-        up2s3(table)
-        ups3time = int( time.time() )
-        runs3 = ups3time - csvtime
+def main(table):
+    start = time.time()
+    dbcreds = open('creds/dbcreds.txt', 'r').read().split('\n')
+    dbconn = mysql.connect(dbcreds[0], dbcreds[1], dbcreds[2], dbcreds[3])
+    cur = dbconn.cursor()  
+    # get the description
+    cur.execute("describe %s" % table)
+    description = cur.fetchall()
+    # connect to redshift and copy the file that we just uploaded to s3 to redshift
+    engine = create_engine('postgresql+psycopg2://', creator=create_conn)
+    redconn = engine.connect()
+    columns = create_query(description)
  
-        # create the table with the columns query now generated
+    try:
+        redconn.execute("create table {0}({1})".format(table, columns))
+    except:
+        delete_current = raw_input("%s already exists\nDo you want to delete rewrite?: " % table)
+        if delete_current.lower() == 'y' or delete_current.lower() == 'yes':
+            redconn.execute("drop table %s" % table)
+            time.sleep(1)
+            redconn.execute("create table {0}({1})".format(table, columns))
+        elif delete_current.lower() == 'n' or delete_current.lower() == 'no':
+            print "Process aborted"
+            exit()
+    
+    write2csv(table, cur)
+     
+    csvtime = int( time.time() )
+    runcsv = csvtime - start
 
-        s3creds = open('creds/s3creds.txt', 'r').read().split('\n')
-        access_key = s3creds[0]
-        secret_key = s3creds[1]
-        try:
-            redconn.execute("COPY {0} FROM 's3://redxfer/{0}' CREDENTIALS 'aws_access_key_id={1};aws_secret_access_key={2}' delimiter '|'".format(table, access_key, secret_key))
-        except:
-           # step through the csv we are about to copy over and change the encodings to work propely with redshift
-           print "Rewriting file...."
-   
-           with open('%s.csv' % table, 'r') as csvfile:
-               reader = csv.reader(csvfile, delimiter='|')
-               with open('%s2.csv' % table, 'wb') as csvfile2:
-                   writer = csv.writer(csvfile2, delimiter='|')
-                   keep_going = True
-                   while keep_going:
-                       try:
-                           this = reader.next()
-                           new = [ i.decode('latin-1').encode('utf-8') for i in this ]
-                           writer.writerow(new)
-                       except StopIteration:
-                           keep_going = False
-
-           print "Rewrite complete"
-           os.remove('%s.csv' % table)
-           os.system("mv {0}2.csv {0}.csv".format(table))
-           up2s3(table)
-           # atomicity insurance
-           time.sleep(10)
-           redconn.execute("COPY {0} FROM 's3://redxfer/{0}' CREDENTIALS 'aws_access_key_id={1};aws_secret_access_key={2}' delimiter '|'".format(table, access_key, secret_key))
+    up2s3(table)
+    ups3time = int( time.time() )
+    runs3 = ups3time - csvtime
+ 
+    # create the table with the columns query now generated
+    
+    s3creds = open('creds/s3creds.txt', 'r').read().split('\n')
+    access_key = s3creds[0]
+    secret_key = s3creds[1]
+    try:
+        redconn.execute("COPY {0} FROM 's3://redxfer/{0}' CREDENTIALS 'aws_access_key_id={1};aws_secret_access_key={2}' delimiter '|'".format(table, access_key, secret_key))
+    except:
+       # step through the csv we are about to copy over and change the encodings to work propely with redshift
+       print "Rewriting file...."
        
-        redtime = int( time.time() )
-        redrun = redtime - ups3time
-        totalrun = redtime - start       
-        print "Successfully copied %s from RDS to S3 to Redshift\n\n" % table
-        print "Metrics:\n\t{0} seconds to write csv\n\t{1} seconds to write to s3\n\t{2} seconds to copy from s3 to redshift\n\t{3} seconds to complete entire process".format( str(runcsv), str(runs3), str(redrun), str(totalrun) )
+       with open('%s.csv' % table, 'r') as csvfile:
+           reader = csv.reader(csvfile, delimiter='|')
+           with open('%s2.csv' % table, 'wb') as csvfile2:
+               writer = csv.writer(csvfile2, delimiter='|')
+               keep_going = True
+               while keep_going:
+                   try:
+                       this = reader.next()
+                       new = [ i.decode('latin-1').encode('utf-8') for i in this ]
+                       writer.writerow(new)
+                   except StopIteration:
+                       keep_going = False
+    
+       print "Rewrite complete"
+       os.remove('%s.csv' % table)
+       os.system("mv {0}2.csv {0}.csv".format(table))
+       up2s3(table)
+       # atomicity insurance
+       time.sleep(10)
+       redconn.execute("COPY {0} FROM 's3://redxfer/{0}' CREDENTIALS 'aws_access_key_id={1};aws_secret_access_key={2}' delimiter '|'".format(table, access_key, secret_key))
+   
+    redtime = int( time.time() )
+    redrun = redtime - ups3time
+    totalrun = redtime - start       
+    print "Successfully copied %s from RDS to S3 to Redshift\n\n" % table
+    print "Metrics:\n\t{0} seconds to write csv\n\t{1} seconds to write to s3\n\t{2} seconds to copy from s3 to redshift\n\t{3} seconds to complete entire process".format( str(runcsv), str(runs3), str(redrun), str(totalrun) )
 
-    else:
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
         print "Supply a table argument"
-
+    else:
+        table = sys.argv[1]
+        main(table)
  
