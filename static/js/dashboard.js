46c32e708b2e1883b162cef511d4c0aaa3123a37
Japhy Bartlett <japhy@edgeflip.com>
2013-10-16 02:51:38 -0500

    styling

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -205,7 +205,7 @@ function mkdaily (response) {
     $('#tsver').off('click').on('click', function() {
         $.post('/alldata/', {campaign:window.campaign_id}, tsv_data);
         })
-
+    $('#tsver').show();
     }
 
 
@@ -263,6 +263,8 @@ function mkgraph(element, response) {
 
 
 function sort() {
+    /* take a click on a table header and sort the summary rows */
+
     var metric = this.id;
     if (metric == window.metric) {
         // toggling sort order if they've clicked the same metric
@@ -337,113 +339,6 @@ function tsv_data(response) {
         $('#modal #datepicker').show();
         $('#tsver span').text('Show Raw Data');
         $('#tsver').off('click').on('click', tsv_data);
-        });
-    }
-
-
-/* Cruft below ? */
-
-
-function getData() {
-    // no need to post client, the server can pick that out of whatever user
-    day = $.datepicker.formatDate('mm/dd/yy', $('#datepicker').datepicker('getDate'));
-    campaign = $('#campaignpicker select').val();
-
-    $.post('/dailydata/', {'campaign':campaign, 'day':day}, onData);
-    }
-
-
-function onData(response) {
-    window.response = response;
-
-    // reset valid dates in the jquery widget
-    $('#datepicker').datepicker( "option", "minDate", response.minday);
-    $('#datepicker').datepicker( "option", "maxDate", response.maxday);
-
-
-    // update data tables
-    window.dailydata = new google.visualization.DataTable({'cols':response.daily_cols, 'rows':response.daily});
-    window.monthlydata = new google.visualization.DataTable({'cols':response.monthly_cols, 'rows':response.monthly});
-
-
-    // format dates
-    var shortdate = new google.visualization.DateFormat({formatType: 'short'});
-    shortdate.format( monthlydata, 0); 
-
-    var shorthour = new google.visualization.DateFormat({formatType: 'HH'});
-    shorthour.format( dailydata, 0);
-
-    draw();
-    }
-
-
-function draw() {
-    $('label').show();
-    window.chart?drawcharts():drawtables();
-    }
-
-
-function drawtables() {
-    window.dailychart.clearChart();  // maybe unnecessary actually
-    window.monthlychart.clearChart();
-
-    // hide the datepicker widget, it's awkward in the middle
-    $('#datepicker').hide();
-
-    window.dailychart = new google.visualization.Table( $('#daily')[0] );
-    window.dailychart.draw(window.dailydata, {
-        title: 'Hourly Volume - '+window.response.dailyday, 
-        titleTextStyle: {fontSize:18},
-        });
-
-    window.monthlychart = new google.visualization.Table( $('#monthly')[0] );
-    window.monthlychart.draw(window.monthlydata, {
-        title: 'Daily Volume '+response.minday+' - '+response.maxday, 
-        titleTextStyle: {fontSize:18},
-        });
-    }
-
-
-function drawcharts() {
-
-    // turn on our datepicker if we're coming from tables
-    $('#datepicker').show();
-
-    // redraw charts
-    var dailyopts = {
-        title: 'Hourly Volume - '+window.response.dailyday, 
-        titleTextStyle: {fontSize:18},
-        enableInteractivity: 'true', 
-        width: 580, 
-        height:175,
-        chartArea: {width:500},
-        lineWidth: 1, // set this a bit thinner than monthly 
-
-        // curveType: 'function', // nice curves but it messes up vaxis calcs
-        legend: {position: 'none'}, // kill this legend, we'll hack into the bigger one below
-        // "turn off" the gridlines, but keep unit labels on axes
-        vAxis: {gridlines: {color:'#FFF'} },
-        hAxis: {gridlines: {color:'#FFF', count:13}, format:'H', title:'Time', titleTextStyle:{fontSize:13}},
-        };
-
-    window.dailychart = new google.visualization.LineChart( $('#daily')[0] );
-    window.dailychart.draw(window.dailydata, dailyopts);
-
-    // window.dailychart = new google.visualization.LineChart( $('#daily')[0] );
-
-    window.monthlychart = new google.visualization.LineChart( $('#monthly')[0] );
-    window.monthlychart.draw(window.monthlydata, {
-        title: 'Daily Volume '+response.minday+' - '+response.maxday, 
-        titleTextStyle: {fontSize:18},
-        enableInteractivity: 'true', 
-        chartArea:{left:50,},
-        width: 799,
-        height:250,
-        legend: {position: 'right', textStyle: {fontSize:10}, alignment: 'end'},
-
-        // "turn off" the gridlines, but keep unit labels on axes
-        vAxis: {gridlines: {color:'#FFF'}, logScale:window.logscale},
-        hAxis: {gridlines: {color:'#FFF', count:7}, format:'M/dd', title:'Date', titleTextStyle:{fontSize:13}},
         });
     }
 
