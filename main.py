3d5b41533f42f66191f4730e1deea8fb1006f145
Japhy Bartlett <japhy@edgeflip.com>
2013-10-17 19:06:53 -0500

    clear out cruft

--- main.py
+++ main.py
@@ -39,7 +39,6 @@ class App(tornado.web.Application):
             (r"/dailydata/", DailyData),
             (r"/hourlydata/", HourlyData),
             (r"/alldata/", AllData),
-            (r"/summary/?", Summary),  # summary data by internal campaign_id, broken now
             (r"/login/", Login),
             (r"/logout/", Logout),
         ]
@@ -80,20 +79,13 @@ class MainHandler(AuthMixin, tornado.web.RequestHandler):
     @tornado.web.authenticated
     def get(self):
 
+        """Basically just loads template.. why does this query for campaigns?"""
+
         ctx = {
             'STATIC_URL':'/static/',
             'user': self.get_current_user(),
             'updated': self.application.updated,
         }
-
-        # look up campaigns
-        q = """
-            SELECT campaign_id, name FROM campaigns WHERE client_id=2 AND campaign_id IN
-                (SELECT DISTINCT(root_id) FROM campchain)
-            ORDER BY campaign_id DESC;
-            """
-        self.application.pcur.execute(q)
-        ctx['campaigns'] = self.application.pcur.fetchall()
 
         return self.render('clientsum.html', **ctx)
 
@@ -126,100 +118,6 @@ class ClientSummary(AuthMixin, tornado.web.RequestHandler):
         """, (client,))
 
         return self.finish(json.dumps([dict(row) for row in self.application.pcur.fetchall()]))
-
-
-class Summary(AuthMixin, tornado.web.RequestHandler):
-
-    @tornado.web.authenticated
-    def get(self):
-
-        ctx = {
-            'STATIC_URL':'/static/',
-            'user': self.get_current_user(),
-            'updated': self.application.updated,
-            'defaultdict': defaultdict,
-        }
-
-        ctx.update( self.aggregate())
-
-        return self.render('sumtable.html', **ctx)
-
-    def aggregate(self, client=2):
-
-        # get all the summary data
-        self.application.pcur.execute("""
-            SELECT meta.campaign_id, meta.name, visits, clicks, auths, uniq_auths,
-                        shown, shares, audience, clickbacks
-            FROM
-                (SELECT campaign_id, SUM(visits) AS visits, SUM(clicks) AS clicks, SUM(auths) AS auths,
-                        SUM(uniq_auths) AS uniq_auths, SUM(shown) AS shown, SUM(shares) AS shares,
-                        SUM(audience) AS audience, SUM(clickbacks) AS clickbacks
-                    FROM clientstats
-                    GROUP BY campaign_id
-                ) AS stats,
-    
-                (SELECT campaign_id, name FROM campaigns WHERE client_id=%s) AS meta
-    
-            WHERE stats.campaign_id=meta.campaign_id
-            ORDER BY meta.campaign_id DESC;
-        """, (client,))
-
-        # index by campaign_id
-        aggdata = {row['campaign_id']:dict(row) for row in self.application.pcur.fetchall()}
-
-        # look up root campaigns
-        self.application.pcur.execute("""
-            SELECT root_id,parent_id
-            FROM campchain 
-    
-            LEFT JOIN 
-                (SELECT DISTINCT(campaign_id) FROM campaigns WHERE client_id=%s) as clientcampaigns
-            ON clientcampaigns.campaign_id=campchain.parent_id
-    
-            WHERE parent_id IS NOT NULL
-            ORDER BY root_id DESC, parent_id DESC;
-        """, (client,))
-
-        # build chains as lists
-        chains = defaultdict(lambda: [])
-        for row in self.application.pcur.fetchall():
-            chains[row['root_id']].append( row['parent_id'])
-
-        # but we need them ordered.. so this is probably silly
-        chainkeys = chains.keys()
-        chainkeys.sort()
-        chainkeys.reverse()
-
-        # build summary data per root
-        sumdata = {}
-        for root in chains:
-            d = defaultdict(lambda:[])
-            for child in chains[root]:
-                if child not in aggdata: continue
-                _data = aggdata[child]
-                [d[k].append(_data[k]) for k in _data]
-            del d['name']
-
-            sumdata[root] = {k:sum(d[k]) for k in d}
-
-        # grab campaign metadata
-        self.application.pcur.execute("""
-        SELECT campaign_id, name FROM campaigns WHERE client_id=%s
-        """, (client,))
-        campmeta = {row['campaign_id']:dict(row) for row in self.application.pcur.fetchall()}
-
-        """
-        # GOOGlify it
-        aggdata = []
-        for row in self.application.pcur.fetchall():
-            aggdata.append( {'c': [{'v':x} for x in row[1:]]})
-        """
-   
-        from views import MONTHLY_METRICS 
-        metrics = MONTHLY_METRICS[:]
-        metrics[0] = {'type':'string', 'id':'campname', 'label':'Campaign Name'}
-
-        return {'data':aggdata, 'sumdata':sumdata, 'chains':chains, 'chainkeys':chainkeys, 'cols':metrics, 'meta':campmeta}
 
 
 import datetime
