c15f72c4d076397a1a960d135472c16c20c1c5d3
Japhy Bartlett <japhy@edgeflip.com>
2013-10-23 11:40:12 -0500

    starting on hooking up django auth

--- auth.py
+++ auth.py
@@ -1,4 +1,8 @@
 import tornado.web
+import base64
+
+from django.utils.crypto import pbkdf2
+# from django.contrib.auth import PBKDF2PasswordHasher
 
 class Logout(tornado.web.RequestHandler):
     def get(self, *args):
@@ -8,18 +12,30 @@ class Logout(tornado.web.RequestHandler):
 
 class Login(tornado.web.RequestHandler):
     def get(self, *args):
-        self.render("login.html", errors=None) 
+        self.render("login.html", error=None) 
 
     def post(self):
+        # check that this user exists, get password string to compare
         login = self.get_argument('login')  # this will error if there's no login arg, iirc
+
         password = self.get_argument('password')
 
         if (password != 'sharing'):
-            return self.render("login.html", errors='Wrong username or password.')
+            return self.render("login.html", error='Wrong username or password.')
 
         self.set_secure_cookie('user', login)  
 
         self.redirect('/')
+
+    # swiped mostly from django.contrib.auth.PBKDF2PasswordHasher
+    def encode(self, password, salt, iterations=None):
+        assert password is not None
+        assert salt and '$' not in salt
+        if not iterations:
+            iterations = self.iterations
+        hash = pbkdf2(password, salt, iterations, digest=self.digest)
+        hash = base64.b64encode(hash).decode('ascii').strip()
+        return "%s$%d$%s$%s" % (self.algorithm, iterations, salt, hash)
 
 
 class AuthMixin(object):
