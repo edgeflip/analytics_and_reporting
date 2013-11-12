81311ba9bb3c5eec1598daa7b1d2ad8409a6cb9e
Japhy Bartlett <japhy@edgeflip.com>
2013-11-12 14:02:15 -0600

    js prettifying, more notes, snip some cruft

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -4,25 +4,32 @@ function init() {
     // first pageview, get summary data and draw main table
     mksummary();
 
-    // default sorting metric
+    // set default sorting metric, bind click handler
     window.metric = 'visits';
     $('#sumtable th').click(sort);
 
     // though we bind the sort handler to the entire th, style the actual buttons
-    $('.sorter').button( {'icons':{'secondary':'ui-icon-arrow-2-n-s'}, 'text':true});
-    }
+    $('.sorter').button({
+        'icons': {
+            'secondary': 'ui-icon-arrow-2-n-s'
+        },
+        'text': true
+    });
+}
 
-
-// FIRST VIEW, THE LEADERBOARD
 
 function mksummary() {
-    // initial load of summary data
-
-    $('#clientpicker').attr('disabled', 'disabled');
+    // initial load of summary data, the first view
     
-    var client_id =  $('#clientpicker').length == 1 ? $('#clientpicker option:selected').val() : 0;
+    // if the clientpicker is on, disable it while we're loading data to stop async races
+    $('#clientpicker').attr('disabled', 'disabled');
 
-    $.get("/tabledata/", {'client':client_id}, function (response) {
+    // send a client_id if we're a superuser, else send 0 and the server will check auths 
+    var client_id = $('#clientpicker').length == 1 ? $('#clientpicker option:selected').val() : 0;
+
+    $.get("/tabledata/", {
+        'client': client_id
+    }, function(response) {
 
         // turn off our loading gif
         $('.loading').hide();
@@ -43,28 +50,41 @@ function mksummary() {
             .enter()
             .append("tr")
             .attr("class", "child")
-            .attr("id", function(d) {return d.root_id;});
+            .attr("id", function(d) {
+                return d.root_id;
+            });
 
         // build cells per row
         rows.selectAll("td").data(
             /* so for each row, we end up wanting an array of values, in column order */
             function(row) {
                 // columns.map makes a nice [] of datapoints per row
-                return columns.map(function(column) {return row[column];});
+                return columns.map(function(column) {
+                    return row[column];
+                });
             })
             .enter()
             .append("td")
-            .text(function(d){return d;})
+            .text(function(d) {
+                return d;
+            })
             .attr("class", "datapoint");
 
         // and a chart-toggler at the end
         rows.append("td")
             .append("button")
             .attr("class", "charter")
-            .attr("root-id", function(d){return d.root_id;});
+            .attr("root-id", function(d) {
+                return d.root_id;
+            });
 
         // make them jquery buttons
-        $('.charter').button( {'icons':{'primary':'ui-icon-image'}, 'text':false});
+        $('.charter').button({
+            'icons': {
+                'primary': 'ui-icon-image'
+            },
+            'text': false
+        });
         // bind a click function for charts
         $('button.charter').click(mkchart);
 
@@ -74,177 +94,15 @@ function mksummary() {
             .data(columns)
             .enter()
             .append("td")
-            .text( function(metric,i) {return i===0?'TOTALS':d3.sum(window.response.map(function(d){return d[metric];}));})
+            .text(function(metric, i) {
+                return i === 0 ? 'TOTALS' : d3.sum(window.response.map(function(d) {
+                    return d[metric];
+                }));
+            })
             .attr("class", "datapoint");
 
-        });
-    }
-
-
-// CAMPAIGN DETAILS SPAWNED FROM BUTTON PER ROW 
-
-function mkchart () {
-    // on click of a campaign, get more detailed data and draw some charts
-    $.post('/alldata/', {'campaign':$(this).attr('root-id')}, on_hourly );
-
-    // stash this so other UI elements know which campaign is selected
-    window.campaign_id = $(this).attr('root-id');
-
-    // open a blank modal so the user knows the button click registered
-    $('#modal').dialog({'modal':true, 'width':1000});  // pass height if you need to
-    $('#modal').on( "dialogclose", function() {
-        // clear old data
-        $('.chart').children().remove();
-        $('#hourlytable tbody').children().remove();
-        $('#legend').children().remove();
-        $('#slider').children('a').remove();
-        $('#xAxis').children().remove();
-        $('#yAxis').children().remove();
-
-        // reset the modal to show the loading spinner
-        $('#hourlytable').hide();
-        $('#chart_container').hide();
-        $('#tsver').hide();
-        $('#modal .loading').show();
-        });
-
-    $('#modal .loading').show();
-    }
-
-
-function on_hourly (response) {
-    // receipt of hourly data for this campaign, draw some charts
-    window.response = response;
-
-    $('#modal .loading').hide();
-    $('#tsver').show();
-
-    // many things use this
-    window.daterange = window.response.data.map( function(row){return new Date(row.time);} );
-
-    $('#hourlygraph').children().remove();
-    var graph = mkgraph('#graph', response);
-
-    // reveal chart container
-    $('#chart_container').show();
-
-
-    // the control for revealing TSV data
-    $('#tsver').button();
-    $('#tsver').off('click').on('click', tsv_data);
-    $('#tsver').show();
-
-    }
-
-
-function mkgraph(element, response) {
-
-    var palette = new Rickshaw.Color.Palette();
-
-    var graph = new Rickshaw.Graph( {
-        element: document.querySelector(element),
-        width: 600,
-        height: 300,
-        renderer: 'line',
-
-        // very ugly, couldn't get the clojure working right
-        series: [{
-                name: "Visits",
-                data: response.data.map(function(row) {return {x:new Date(row.time).getTime()/1000, y:row.visits};}),
-                color: palette.color(),
-            },{
-                name: "Clicks",
-                data: response.data.map(function(row) {return {x:new Date(row.time).getTime()/1000, y:row.clicks};}),
-                color: palette.color(),
-            },{
-                name: "Unique Auths",
-                data: response.data.map(function(row) {return {x:new Date(row.time).getTime()/1000, y:row.uniq_auths};}),
-                color: palette.color(),
-            },{
-                name: "Faces Shown",
-                data: response.data.map(function(row) {return {x:new Date(row.time).getTime()/1000, y:row.shown};}),
-                color: palette.color(),
-            },{
-                name: "Shares",
-                data: response.data.map(function(row) {return {x:new Date(row.time).getTime()/1000, y:row.shares};}),
-                color: palette.color(),
-            },{
-                name: "Audience",
-                data: response.data.map(function(row) {return {x:new Date(row.time).getTime()/1000, y:row.audience};}),
-                color: palette.color(),
-            },{
-                name: "Clickbacks",
-                data: response.data.map(function(row) {return {x:new Date(row.time).getTime()/1000, y:row.clickbacks};}),
-                color: palette.color(),
-            },]
-        });
-
-    window.graph = graph;
-
-    new Rickshaw.Graph.HoverDetail({ graph: graph, yFormatter: function (x) {return x;} });
-
-    var legend = new Rickshaw.Graph.Legend( {
-        graph: graph,
-        element: document.getElementById('legend')
-        } );
-
-    var highlight = new Rickshaw.Graph.Behavior.Series.Highlight( {
-        graph: graph,
-        legend: legend
-        } );
-
-    var shelving = new Rickshaw.Graph.Behavior.Series.Toggle( {
-        graph: graph,
-        legend: legend
-        } );
-
-    var slider = new Rickshaw.Graph.RangeSlider( {
-        graph: graph,
-        element: document.getElementById('slider')
-        } );
-
-    graph.render();
-
-    // new Rickshaw.Graph.Axis.Time( { graph: graph, timeUnit:time.unit('day') } );
-    // var xAxis = new Rickshaw.Graph.Axis.Time({ 
-    var xAxis = new Rickshaw.Graph.Axis.X({
-        graph: graph,
-        ticks: 4,
-        orientation: 'bottom',
-        element: document.getElementById('xAxis'),
-        tickFormat: function(x) {
-            // find range to display either dates or times .. ideally calc this not for every tick (once per update)
-            var stacktimes = graph.series[0].stack.map( function(row) {return row.x;});
-            var tdelta = d3.max(stacktimes) - d3.min(stacktimes);
-
-            var d = new Date(x*1000);
-            return tdelta > 86400 ? d.toLocaleDateString() : d.toLocaleTimeString();
-            }
-        });
-    xAxis.render();
-
-    var yAxis = new Rickshaw.Graph.Axis.Y({
-        graph: graph,
-        orientation: 'left',
-        ticks: 4,
-        element: document.getElementById('yAxis'),
-        });
-    yAxis.render();
-
-    // permanent X min and maxes
-    var extents = d3.extent( response.data, function(row) {return new Date(row.time);});
-    $('#xMin').text( extents[0].toLocaleDateString());
-    $('#xMax').text( extents[1].toLocaleDateString());
-
-    // window Y min and max
-    graph.onUpdate( function() {
-        yMax = d3.max( graph.series.map( function(row) { return d3.max( row.stack.map( function(r) {return r.y;}));}));
-        $('#yMax').text(yMax);
-        });
-
-    return graph;
-
-    }
+    });
+}
 
 
 function sort() {
@@ -254,11 +112,10 @@ function sort() {
     if (metric == window.metric) {
         // toggling sort order if they've clicked the same metric
         window.sorter = window.sorter === d3.ascending ? d3.descending : d3.ascending;
-        }
-    else {
+    } else {
         // else a new metric, set to descending by default
         window.sorter = d3.descending;
-        }
+    }
 
     var sortstyle = window.sorter === d3.descending ? 'descend' : 'ascend';
 
@@ -266,22 +123,253 @@ function sort() {
     $('th').removeClass('ascend descend');
 
     // reset button styles
-    $('.sorter').button( {'icons':{'secondary':'ui-icon-arrow-2-n-s'}, 'text':true});
+    $('.sorter').button({
+        'icons': {
+            'secondary': 'ui-icon-arrow-2-n-s'
+        },
+        'text': true
+    });
 
     // set new styles
-    $('.tableFloatingHeaderOriginal #'+metric).addClass(sortstyle);
-    $('.tableFloatingHeader #'+metric).addClass(sortstyle);
-    $(this).addClass( sortstyle);
+    $('.tableFloatingHeaderOriginal #' + metric).addClass(sortstyle);
+    $('.tableFloatingHeader #' + metric).addClass(sortstyle);
+    $(this).addClass(sortstyle);
 
-    // set new button icon
-    var toggle = sortstyle == 'descend' ? 'ui-icon-arrowthick-1-s': 'ui-icon-arrowthick-1-n';
-    $('.tableFloatingHeaderOriginal #'+metric+' .sorter').button('option', 'icons', {secondary:toggle});
-    $('.tableFloatingHeader #'+metric+' .sorter').button('option', 'icons', {secondary:toggle});
+    // set new button icon (up or down), on floating header _and_ original
+    var toggle = sortstyle == 'descend' ? 'ui-icon-arrowthick-1-s' : 'ui-icon-arrowthick-1-n';
+    $('.tableFloatingHeaderOriginal #' + metric + ' .sorter').button('option', 'icons', {
+        secondary: toggle
+    });
+    $('.tableFloatingHeader #' + metric + ' .sorter').button('option', 'icons', {
+        secondary: toggle
+    });
 
     window.metric = metric;
 
-    d3.selectAll("tr.child").sort(function(a,b) {return window.sorter(a[metric],b[metric]);});
-    }
+    // and.. actually sort the data
+    d3.selectAll("tr.child").sort(function(a, b) {
+        return window.sorter(a[metric], b[metric]);
+    });
+}
+
+
+// CAMPAIGN DETAILS SPAWNED FROM BUTTON PER ROW 
+
+function mkchart() {
+    /* on click of a campaign chart button, fetch hourly data points and draw some charts */
+
+    $.post('/alldata/', {
+        'campaign': $(this).attr('root-id')
+    }, on_hourly);
+
+    // stash this so other UI elements know which campaign is selected
+    window.campaign_id = $(this).attr('root-id');
+
+    // open a blank modal so the user knows the button click registered
+    $('#modal').dialog({
+        'modal': true,
+        'width': 1000
+    }); // pass height if you need to
+    $('#modal').on("dialogclose", function() {
+        /* TODO: this could be more graceful with more clever naming, probably       
+         * but on a closing of the modal popup, wipe out old things and reset the
+         * state of the modal to blank/loading
+         */
+
+        // clear old data if it exists (viewing one chart, then another)
+        $('.chart').children().remove();
+        $('#hourlytable tbody').children().remove();
+        $('#legend').children().remove();
+        $('#slider').children('a').remove();
+        $('#xAxis').children().remove();
+        $('#yAxis').children().remove();
+        $('#hourlygraph').children().remove();
+
+        // reset the modal to show the loading spinner
+        $('#hourlytable').hide();
+        $('#chart_container').hide();
+        $('#tsver').hide();
+        $('#modal .loading').show();
+    });
+}
+
+
+function on_hourly(response) {
+    // receipt of hourly data for this campaign, draw some charts
+    window.response = response;
+
+    // turn off loading spinner, show the TSV data button
+    $('#modal .loading').hide();
+    $('#tsver').show();
+
+    // stash this data on window, axes and controls use this also
+    window.daterange = window.response.data.map(function(row) {
+        return new Date(row.time);
+    });
+
+    // construct the graphical bits
+    mkgraph('#graph', response);
+
+    // then reveal the parent container
+    $('#chart_container').show();
+
+
+    // the control for revealing TSV data
+    $('#tsver').button();
+    $('#tsver').off('click').on('click', tsv_data);
+}
+
+
+function mkgraph(element, response) {
+
+    var palette = new Rickshaw.Color.Palette();
+
+    var graph = new Rickshaw.Graph({
+        element: document.querySelector(element),
+        width: 600,
+        height: 300,
+        renderer: 'line',
+
+        // Rickshaw wants one array per type of data, so generate each one with a map() on 
+        // the data we've sent from the server.. there is probably a clever way to inspect
+        // the response and do this automagically in one line of code
+        series: [{
+            name: "Visits",
+            data: response.data.map(function(row) {
+                return {
+                    x: new Date(row.time).getTime() / 1000,
+                    y: row.visits
+                };
+            }),
+            color: palette.color(),
+        }, {
+            name: "Clicks",
+            data: response.data.map(function(row) {
+                return {
+                    x: new Date(row.time).getTime() / 1000,
+                    y: row.clicks
+                };
+            }),
+            color: palette.color(),
+        }, {
+            name: "Unique Auths",
+            data: response.data.map(function(row) {
+                return {
+                    x: new Date(row.time).getTime() / 1000,
+                    y: row.uniq_auths
+                };
+            }),
+            color: palette.color(),
+        }, {
+            name: "Faces Shown",
+            data: response.data.map(function(row) {
+                return {
+                    x: new Date(row.time).getTime() / 1000,
+                    y: row.shown
+                };
+            }),
+            color: palette.color(),
+        }, {
+            name: "Shares",
+            data: response.data.map(function(row) {
+                return {
+                    x: new Date(row.time).getTime() / 1000,
+                    y: row.shares
+                };
+            }),
+            color: palette.color(),
+        }, {
+            name: "Audience",
+            data: response.data.map(function(row) {
+                return {
+                    x: new Date(row.time).getTime() / 1000,
+                    y: row.audience
+                };
+            }),
+            color: palette.color(),
+        }, {
+            name: "Clickbacks",
+            data: response.data.map(function(row) {
+                return {
+                    x: new Date(row.time).getTime() / 1000,
+                    y: row.clickbacks
+                };
+            }),
+            color: palette.color(),
+        }, ]
+    });
+
+    // turn on the various Rickshaw features
+
+    // Hover details
+    new Rickshaw.Graph.HoverDetail({
+        graph: graph,
+        yFormatter: function(x) {
+            return x;
+        }
+    });
+
+    // Legend
+    var legend = new Rickshaw.Graph.Legend({
+        graph: graph,
+        element: document.getElementById('legend')
+    });
+
+    // Legend controls
+    var highlight = new Rickshaw.Graph.Behavior.Series.Highlight({
+        graph: graph,
+        legend: legend
+    });
+
+    // Legend controls
+    var shelving = new Rickshaw.Graph.Behavior.Series.Toggle({
+        graph: graph,
+        legend: legend
+    });
+
+    // The slider control for selecting start/end times
+    var slider = new Rickshaw.Graph.RangeSlider({
+        graph: graph,
+        element: document.getElementById('slider')
+    });
+
+    graph.render();
+
+    // AXES
+    var xAxis = new Rickshaw.Graph.Axis.X({
+        graph: graph,
+        ticks: 4,
+        orientation: 'bottom',
+        element: document.getElementById('xAxis'),
+        tickFormat: function(x) {
+            // find range to display either dates or times .. ideally calc this not for every tick (once per update)
+            var stacktimes = graph.series[0].stack.map(function(row) {
+                return row.x;
+            });
+            var tdelta = d3.max(stacktimes) - d3.min(stacktimes);
+
+            var d = new Date(x * 1000);
+            return tdelta > 86400 ? d.toLocaleDateString() : d.toLocaleTimeString();
+        }
+    });
+    xAxis.render();
+
+    var yAxis = new Rickshaw.Graph.Axis.Y({
+        graph: graph,
+        orientation: 'left',
+        ticks: 4,
+        element: document.getElementById('yAxis'),
+    });
+    yAxis.render();
+
+    // permanent X min and maxes
+    var extents = d3.extent(response.data, function(row) {
+        return new Date(row.time);
+    });
+    $('#xMin').text(extents[0].toLocaleDateString());
+    $('#xMax').text(extents[1].toLocaleDateString());
+}
+
 
 
 function tsv_data(response) {
@@ -303,11 +391,15 @@ function tsv_data(response) {
         /* so for each row, we end up wanting an array of values, in column order */
         function(row) {
             // columns.map makes a nice [] of datapoints per row
-            return columns.map(function(column) {return row[column];});
+            return columns.map(function(column) {
+                return row[column];
+            });
         })
         .enter()
         .append("td")
-        .text(function(d){return d+'\t';})
+        .text(function(d) {
+            return d + '\t';
+        })
         .attr("class", "datapoint");
 
     // toggle visibility .. kinda awkward
@@ -316,11 +408,10 @@ function tsv_data(response) {
 
     // toggle button functionality
     $('#tsver span').text('Data as Graphs');
-    $('#tsver').off('click').on('click', function () {
+    $('#tsver').off('click').on('click', function() {
         $('#hourlytable').hide();
         $('#chart_container').show();
         $('#tsver span').text('Data as TSV');
         $('#tsver').off('click').on('click', tsv_data);
-        });
-    }
-
+    });
+}
