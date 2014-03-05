c1f68a01c07b6f4d470613513f789bd34041e8f2
mattratt <mattratt@gmail.com>
2014-03-04 22:40:16 -0500

    added multiprocessing, but it don't seem to go less slow

--- parse_fb_json/parse_fb_json.py
+++ parse_fb_json/parse_fb_json.py
@@ -1,14 +1,21 @@
 #!/usr/bin/python
 import sys
+import os
 import json
 from boto.s3.connection import S3Connection
 from urlparse import urlparse
+from tempfile import mkstemp
 import argparse
+import multiprocessing
+
+
 
 
 AWS_ACCESS_KEY = "AKIAJDPO2KQRLOJBQP3Q"
 AWS_SECRET_KEY = "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"
 AWS_BUCKET_NAMES = [ "feed_crawler_%d" % i for i in range(5) ]
+
+
 
 
 def get_conn_s3(key=AWS_ACCESS_KEY, sec=AWS_SECRET_KEY):
@@ -19,9 +26,7 @@ def feed_json_iter(bucket_names=AWS_BUCKET_NAMES):
     for b, bucket_name in enumerate(bucket_names):
         sys.stderr.write("reading bucket %d/%d (%s)\n" % (b, len(bucket_names), bucket_name))
 
-        bucket = conn.get_bucket(bucket_name)
-        bucket_keys = [k for k in bucket.list()]
-        for k, key in enumerate(bucket_keys):
+        for k, key in enumerate(conn.get_bucket(bucket_name).list()):
             # name should have format primary_secondary; e.g., "100000008531200_1000760833"
             prim_id, sec_id = map(int, key.name.split("_"))
             feed_raw = key.get_contents_as_string()
@@ -33,7 +38,7 @@ def feed_json_iter(bucket_names=AWS_BUCKET_NAMES):
                 sys.stderr.write("no data in feed %s\n" % key.name)
                 continue
 
-            sys.stderr.write("\t%d/%d read feed json with %d posts from %s\n" % (k, len(bucket_keys), len(feed_json_list), key.name))
+            sys.stderr.write("\t%d read feed json with %d posts from %s\n" % (k, len(feed_json_list), key.name))
             yield Feed(sec_id, feed_json_list)
 
 class Feed(object):
@@ -64,6 +69,7 @@ class Feed(object):
                 link_line = "\t".join([f.encode('utf8', 'ignore') for f in link_fields])
                 outfile_links.write(link_line + "\n")
 
+    @staticmethod
     def write_labels(outfile_posts, outfile_links, delim="\t"):
         # these MUST match field order above
         post_fields = ['user_id', 'post_id', 'post_ts', 'post_type', 'post_app', 'post_from',
@@ -101,6 +107,15 @@ class FeedPost(object):
         if ('comments' in post_json):
             self.comment_ids.update([user['id'] for user in post_json['comments']['data']])
 
+def handle_feed(feed):
+    fd_posts, filename_posts = mkstemp()
+    fd_links, filename_links = mkstemp()
+    with open(filename_posts, 'wb') as outfile_posts, open(filename_links, 'wb') as outfile_links:
+        feed.write(outfile_posts, outfile_links)
+    os.close(fd_posts)
+    os.close(fd_links)
+    return filename_posts, filename_links
+
 
 
 
@@ -109,8 +124,10 @@ class FeedPost(object):
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description='Eat up the FB sync data and put it into a tsv')
-    parser.add_argument('post_file', type=str, help='output file for feed posts')
-    parser.add_argument('link_file', type=str, help='output file for user-post links (like, comm, etc.')
+    parser.add_argument('post_file', type=str, help='out file for feed posts')
+    parser.add_argument('link_file', type=str, help='out file for user-post links (like, comm)')
+    parser.add_argument('--workers', type=int, help='number of workers to multiprocess', default=1)
+    parser.add_argument('--maxfeeds', type=int, help='bail after x feeds are done', default=None)
     args = parser.parse_args()
 
     outfile_posts = open(args.post_file, 'wb')
@@ -118,7 +135,15 @@ if __name__ == '__main__':
 
     Feed.write_labels(outfile_posts, outfile_links)
 
-    for i, feed in enumerate(feed_json_iter()):
+    pool = multiprocessing.Pool(args.workers)
+    for i, (fn_posts, fn_links) in enumerate(pool.imap(handle_feed, feed_json_iter())):
         if (i % 1000 == 0):
             sys.stderr.write("\t%d\n" % i)
-        feed.write(outfile_posts, outfile_links)
+        outfile_posts.write(open(fn_posts).read())
+        os.remove(fn_posts)
+        outfile_links.write(open(fn_links).read())
+        os.remove(fn_links)
+
+        if (args.maxfeeds is not None) and (i >= args.maxfeeds):
+            sys.exit("bailing")
+
