081e08f5756f32e14432f5dc07c7d9b3df74d2b9
mattratt <mattratt@gmail.com>
2014-03-05 19:34:23 -0500

    pushed the reading from s3 down to the child processes; finally getting some parallel love

--- parse_fb_json/parse_fb_json.py
+++ parse_fb_json/parse_fb_json.py
@@ -5,7 +5,7 @@ import json
 import logging
 from boto.s3.connection import S3Connection
 from urlparse import urlparse
-from tempfile import mkstemp
+import tempfile
 import argparse
 import multiprocessing
 
@@ -24,25 +24,12 @@ logger = logging.getLogger(__name__)
 def get_conn_s3(key=AWS_ACCESS_KEY, sec=AWS_SECRET_KEY):
     return S3Connection(key, sec)
 
-def feed_json_iter(bucket_names=AWS_BUCKET_NAMES):
+def key_iter(bucket_names=AWS_BUCKET_NAMES):
     conn = get_conn_s3()
     for b, bucket_name in enumerate(bucket_names):
         logger.debug("reading bucket %d/%d (%s)\n" % (b, len(bucket_names), bucket_name))
-
-        for k, key in enumerate(conn.get_bucket(bucket_name).list()):
-            # name should have format primary_secondary; e.g., "100000008531200_1000760833"
-            prim_id, sec_id = map(int, key.name.split("_"))
-            feed_raw = key.get_contents_as_string()
-            #logger.debug("\tread %d from %s\n" % (len(feed_raw), key.name))
-
-            try:
-                feed_json_list = json.loads(feed_raw)['data']
-            except KeyError:
-                logger.debug("no data in feed %s\n" % key.name)
-                continue
-
-            logger.debug("\t%d read feed json with %d posts from %s\n" % (k, len(feed_json_list), key.name))
-            yield Feed(sec_id, feed_json_list)
+        for key in conn.get_bucket(bucket_name).list():
+            yield key
 
 class Feed(object):
     def __init__(self, user_id, feed_json_list):
@@ -55,22 +42,6 @@ class Feed(object):
                 logger.debug("error parsing: " + str(post_json) + "\n\n")
                 logger.deubg("full feed: " + str(feed_json_list) + "\n\n")
                 raise
-
-    #def write(self, outfile_posts, outfile_links, delim="\t"):
-    #    for p in self.posts:
-    #        post_fields = [self.user_id, p.post_id, p.post_ts, p.post_type, p.post_app, p.post_from,
-    #                  p.post_link, p.post_link_domain,
-    #                  p.post_story, p.post_description, p.post_caption, p.post_message]
-    #        post_line = delim.join([f.replace(delim, " ").encode('utf8', 'ignore') for f in post_fields])
-    #        outfile_posts.write(post_line + "\n")
-    #
-    #        for user_id in p.to_ids.union(p.like_ids, p.comment_ids):
-    #            has_to = "1" if user_id in p.to_ids else ""
-    #            has_like = "1" if user_id in p.like_ids else ""
-    #            has_comm = "1" if user_id in p.comment_ids else ""
-    #            link_fields = [p.post_id, user_id, has_to, has_like, has_comm]
-    #            link_line = "\t".join([f.encode('utf8', 'ignore') for f in link_fields])
-    #            outfile_links.write(link_line + "\n")
 
     def get_posts_str(self, delim="\t"):
         lines = []
@@ -105,6 +76,21 @@ class Feed(object):
         link_fields = ['post_id', 'user_id', 'to', 'like', 'comment']
         outfile_links.write(delim.join(link_fields) + "\n")
 
+class FeedS3(Feed):
+    def __init__(self, key):
+        # name should have format primary_secondary; e.g., "100000008531200_1000760833"
+        prim_id, sec_id = map(int, key.name.split("_"))
+
+        with tempfile.TemporaryFile() as fp:
+            key.get_contents_to_file(fp)
+            fp.seek(0)
+            try:
+                feed_json_list = json.load(fp)['data']
+            except KeyError:
+                logger.debug("no data in feed %s\n" % key.name)
+                raise
+        logger.debug("\tread feed json with %d posts from %s\n" % (len(feed_json_list), key.name))
+        super(FeedS3, self).__init__(sec_id, feed_json_list)
 
 class FeedPost(object):
     def __init__(self, post_json):
@@ -132,14 +118,8 @@ class FeedPost(object):
         if 'comments' in post_json:
             self.comment_ids.update([user['id'] for user in post_json['comments']['data']])
 
-def handle_feed(feed):
-    #fd_posts, filename_posts = mkstemp()
-    #fd_links, filename_links = mkstemp()
-    #with open(filename_posts, 'wb') as outfile_posts, open(filename_links, 'wb') as outfile_links:
-    #    feed.write(outfile_posts, outfile_links)
-    #os.close(fd_posts)
-    #os.close(fd_links)
-    #return filename_posts, filename_links
+def handle_feed(key):
+    feed = FeedS3(key)
     post_lines = feed.get_posts_str()
     link_lines = feed.get_links_str()
     return post_lines, link_lines
@@ -156,24 +136,22 @@ if __name__ == '__main__':
     parser.add_argument('link_file', type=str, help='out file for user-post links (like, comm)')
     parser.add_argument('--workers', type=int, help='number of workers to multiprocess', default=1)
     parser.add_argument('--maxfeeds', type=int, help='bail after x feeds are done', default=None)
+    parser.add_argument('--logfile', type=str, help='for debugging', default=None)
     args = parser.parse_args()
+
+    if args.logfile is not None:
+        logging.basicConfig(filename=args.logfile, level=logging.DEBUG)
 
     outfile_posts = open(args.post_file, 'wb')
     outfile_links = open(args.link_file, 'wb')
 
     Feed.write_labels(outfile_posts, outfile_links)
 
+    sys.stderr.write("process %d farming out to %d childs\n" % (os.getpid(), args.workers))
     pool = multiprocessing.Pool(args.workers)
-    #for i, (fn_posts, fn_links) in enumerate(pool.imap(handle_feed, feed_json_iter())):
-    #    if i % 1000 == 0:
-    #        logger.debug.write("\t%d\n" % i)
-    #    outfile_posts.write(open(fn_posts).read())
-    #    os.remove(fn_posts)
-    #    outfile_links.write(open(fn_links).read())
-    #    os.remove(fn_links)
-    for i, (post_lines, link_lines) in enumerate(pool.imap(handle_feed, feed_json_iter())):
-        if i % 1000 == 0:
-            logger.debug("\t%d\n" % i)
+    for i, (post_lines, link_lines) in enumerate(pool.imap(handle_feed, key_iter())):
+        if i % 100 == 0:
+            sys.stderr.write("\t%d\n" % i)
         outfile_posts.write(post_lines)
         outfile_links.write(link_lines)
 
