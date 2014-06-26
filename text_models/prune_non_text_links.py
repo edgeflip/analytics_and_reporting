91de5046355fadecca36a4b97764e1b01ab46125
Marc <ubuntu@ip-10-187-18-56.ec2.internal>
2014-06-26 20:04:39 +0000

    forgot mp3, mov files

--- text_models/prune_non_text_links.py
+++ text_models/prune_non_text_links.py
@@ -2,9 +2,9 @@ import os
 
 # Remove all files in directory that end with a bad set of characters
 # Discarding non-text URLs (images, pdfs, ppts)
-bad_file_extensions = ['.jpg', '.jpeg', '.gif', '.pdf', '.ppt', '.png']
+bad_file_extensions = ['.jpg', '.jpeg', '.gif', '.pdf', '.ppt', '.png', '.mp3', '.mov']
 data_dir = '/data/user_links'
 for link in os.listdir(data_dir):
-    if link[-4:] in bad_file_extensions or link[-3:] in bad_file_extensions:
+    if link[-5:] in bad_file_extensions or link[-4:] in bad_file_extensions:
         print('removing {}'.format(link))
         os.remove(data_dir + '/' + link)
