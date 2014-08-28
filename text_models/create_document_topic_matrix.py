9d24f11af378dd4ad7f87c9a173a6984786a9173
Marc <ubuntu@ip-10-187-18-56.ec2.internal>
2014-08-28 14:04:43 +0000

    another helper for topic-based approach

--- text_models/create_document_topic_matrix.py
+++ text_models/create_document_topic_matrix.py
@@ -1,38 +1,105 @@
 from scipy.sparse import lil_matrix, csc_matrix
 from scipy.io import mmwrite
 import joblib
+import re
 import sys
+import os
 
-filename_cache = sys.argv[1]
-topic_proportions_filename = sys.argv[2]
-num_topics = int(sys.argv[3])
-output_filename = sys.argv[4]
+def get_document_to_topic_proportions_dict(topic_proportions_filename_list, strip_document_path=False):
+    '''
+    Given topic_proportions_filename, read in each filename and add to 
+    a dictionary that stores filename -> topic -> proportion
+    '''
+    filename_to_topic_proportions = {} # filename -> topic -> proportion
+    for topic_proportions_filename in topic_proportions_filename_list:
+        topic_proportions_file =  open(topic_proportions_filename, 'r')
+        for line in topic_proportions_file:
+            line = line.strip()
+            if line.startswith('#'):
+                continue
+            
+            # parse out topic and proportion pairs
+            # then parse out filename (which could contain whitespace)            
+            topic_prop_pairs = re.findall('(?:\d+\s+0\.\d+)', line)
+            topic_start_idx = re.search('(?:\d+\s+0\.\d+)', line).start()
+            num_and_filename = line[:topic_start_idx].strip()
+            filename_start_idx = re.search('(?:\d+)', num_and_filename).end()
+            filename = num_and_filename[filename_start_idx:].strip()
+            if filename.startswith('file:'):
+                filename = filename[5:]
+            if strip_document_path:
+                filename = os.path.basename(filename)                
 
-filenames = joblib.load(filename_cache)
+            filename_to_topic_proportions.setdefault(filename, {})
+            for topic_prop_pair in topic_prop_pairs:
+                vals = re.split('\s+', topic_prop_pair)
+                topic_id = int(vals[0])
+                proportion = float(vals[1])
+                filename_to_topic_proportions[filename][topic_id] = proportion
+        topic_proportions_file.close()
+    return filename_to_topic_proportions
 
-# get all topics and proportions for each filename
+def get_document_to_topic_proportions_dict_with_label_list(topic_proportions_filename, label_list):
+    '''
+    This version only works with a single filename input
+    Given topic_proportions_filename, read in each filename and add to  
+    a dictionary that stores filename -> topic -> proportion
+    label_list provides names for each line in the topic_proportions_file in the same order (helpful when null-source shows up)
+    '''
+    filename_to_topic_proportions = {} # filename -> topic -> proportion
+    topic_proportions_file =  open(topic_proportions_filename, 'r')
+    for label, line in zip(label_list, topic_proportions_file):
+        line = line.strip()
+        if line.startswith('#'):
+            continue
+        
+        # parse out topic and proportion pairs
+        # then parse out filename (which could contain whitespace)            
+        topic_prop_pairs = re.findall('(?:\d+\s+0\.\d+)', line)
+        topic_start_idx = re.search('(?:\d+\s+0\.\d+)', line).start()
+        
+        filename = label
 
-filename_to_topic_proportions = {} # filename -> topic -> proportion
-topic_proportions_file =  open(topic_proportions_filename, 'r')
-for line in topic_proportions_file:
-    line = line.strip()
-    if line.startswith('#'):
-        continue
-    vals = line.strip().split('\t')
-    filename = vals[1].split('file:')[1]
-    filename_to_topic_proportions.setdefault(filename, {})
-    for idx in range(2, len(vals), 2):
-        topic_id = int(vals[idx])
-        proportion = float(vals[idx+1])
-        filename_to_topic_proportions[filename][topic_id] = proportion
-topic_proportions_file.close()
+        filename_to_topic_proportions.setdefault(filename, {})
+        for topic_prop_pair in topic_prop_pairs:
+            vals = re.split('\s+', topic_prop_pair)
+            topic_id = int(vals[0])
+            proportion = float(vals[1])
+            filename_to_topic_proportions[filename][topic_id] = proportion
+    topic_proportions_file.close()
+    return filename_to_topic_proportions
 
-# for each filename in the input array of filenames, create rows in a feature matrix
+def document_to_feature_matrix(documents, document_to_topic_proportions, num_topics, output_filename=None):
+    features = lil_matrix((documents.shape[0], num_topics))
+    for row_idx, document in enumerate(documents):
+        if document in document_to_topic_proportions:
+            for topic_id, proportion in document_to_topic_proportions[document].items():
+                features[row_idx, topic_id] = proportion
+    features = csc_matrix(features)
+    if output_filename:
+        mmwrite(output_filename, features)
+    else:
+        return features
 
-features = lil_matrix((filenames.shape[0], num_topics))
-for row_idx, filename in enumerate(filenames):
-    for topic_id, proportion in filename_to_topic_proportions[filename].items():
-        features[row_idx, topic_id] = proportion
 
-features = csc_matrix(features)
-mmwrite(output_filename, features)
+if __name__ == '__main__':
+    document_cache = sys.argv[1]
+    topic_proportions_filename = sys.argv[2]
+    num_topics = int(sys.argv[3])
+    output_filename = sys.argv[4]
+    if len(sys.argv) > 4:
+        topic_proportions_loaded = sys.argv[4]
+    else:
+        topic_proportions_loaded = False
+
+    documents = joblib.load(document_cache)
+
+    # get all topics and proportions for each document
+    if not topic_proportions_loaded:
+        document_to_topic_proportions = get_document_to_topic_proportions_dict([topic_proportions_filename])
+    else:
+        document_to_topic_proportions = joblib.load(topic_proportions_filename)
+
+    # for each filename in the input array of filenames, create rows in a feature matrix
+    # and save to file
+    document_to_feature_matrix(documents, document_to_topic_proportions, num_topics, output_filename)
--- text_models/prediction_from_topics.py
+++ text_models/prediction_from_topics.py
@@ -1,3 +1,4 @@
+from sklearn_prediction_utils import models_report
 from sklearn.cross_validation import KFold
 from sklearn.linear_model import Ridge
 # from sklearn.linear_model import Lasso
@@ -26,8 +27,11 @@ if len(sys.argv) > 2:
 else:
     sample_size = 1000
     
-feature_classes = {1: 'user_posts', 
-                   2: 'from_friend_posts'}
+# feature_classes = {1: 'user_posts', 
+#                    2: 'from_friend_posts',
+#                    3: 'user_links'}
+feature_classes = {3: 'user_links'}
+
 cache_dir = '/data/caches/user_and_from_friend_posts'
 
 test_size = 0.2
@@ -69,75 +73,12 @@ sys.stdout.write('{}\n'.format(y.shape))
 
 features = {}
 for feature_idx in feature_classes:
-    features[feature_idx] = csc_matrix(mmread(cache_dir + '/' + 'topic_proportions_{}_{}_1000.mtx'.format(feature_classes[feature_idx], sample_size)))
+    features[feature_idx] = csc_matrix(mmread(cache_dir + '/' + 'topic_proportions_{}_{}_1000-max.mtx'.format(feature_classes[feature_idx], sample_size)))
 
 X = hstack([feature_matrix for feature_matrix in features.values()])
 sys.stdout.write('{}\n'.format(X.shape))
 sys.stdout.flush()
 
-######## classifiers ##########
-
-def mean_results(results):
-    '''
-    results should be a dictionary of score_name: [value1, ...]
-    '''
-    scores_to_mean_values = {}
-    for score_name, value_list in results.items():
-        scores_to_mean_values[score_name] = 1.0*sum(value_list)/len(value_list)
-    return scores_to_mean_values
-    
-def benchmark(clf_class, params, sparse_flag, grid_flag, score_type):    
-    clf = clf_class(**params)
-    
-    res_scores = {}
-    for train_idxs, test_idxs in KFold(n=sample_size, n_folds=num_folds, random_state=random_state):
-        t_train_classifier_start = time()
-        sys.stdout.write('trying to fit\n')
-        if sparse_flag:
-            clf.fit(X[train_idxs], y[train_idxs])
-        else:
-            clf.fit(X[train_idxs].todense(), y[train_idxs])
-        train_time = time() - t_train_classifier_start
-        sys.stdout.write('done fitting\n')
-        sys.stdout.flush()
-    
-        if grid_flag:
-            sys.stdout.write('{}\n'.format(clf.best_estimator_))
-            sys.stdout.flush()
-    
-        t_test_classifier_start = time()
-        if sparse_flag:
-            preds = clf.predict(X[test_idxs])
-        else:
-            preds = clf.predict(X[test_idxs].todense())
-        test_time = time() - t_test_classifier_start
-    
-        if score_type == 'continuous':
-            r2 = metrics.r2_score(y[test_idxs], preds)
-            r = np.sqrt(r2)
-            mean_abs_err = metrics.mean_absolute_error(y[test_idxs], preds)
-            res_scores.setdefault('R2', []).append(r2)
-            res_scores.setdefault('R', []).append(r)
-            res_scores.setdefault('mean(|err|)', []).append(mean_abs_err)
-            res_scores.setdefault('train_time', []).append(train_time)
-            res_scores.setdefault('test_time', []).append(test_time)
-        else:
-            accuracy = metrics.accuracy_score(y[test_idxs], preds)
-            res_scores.setdefault('accuracy', []).append(accuracy)
-            res_scores.setdefault('train_time', []).append(train_time)
-            res_scores.setdefault('test_time', []).append(test_time)
-        
-    clf_descr = str(clf).split('(')[0] + str(params)
-    return clf_descr, res_scores
-
 sys.stdout.write('Training classifiers\...n')
-
-for model in models:
-    clf_description, clf_performance = benchmark(model[0], model[1], model[2], model[3], score_type)
-    sys.stdout.write('{}\t{}\n'.format(clf_description, clf_performance))
-    sys.stdout.write('Sample size: {}\nNum folds: {}\n'.format(sample_size, num_folds))
-    sys.stdout.write('\t{}\n'.format('\n\t'.join(['{}: {}'.format(k, v) for k, v in 
-                                                    mean_results(clf_performance).items()])))
-    sys.stdout.flush()
-
+models_report(models, X, y, sample_size, score_type)
 sys.stdout.write('\tdone. total time: {}\n'.format(time() - t0))
